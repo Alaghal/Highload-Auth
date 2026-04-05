f4cf3d343282bff4cf73fea8650279362dfffb1c
Andrey <seryakov.andreyv@gmail.com>
2026-04-05 23:52:15 +0300

    [ISSUE-8]: upgrade Kotlin, Gradle, and server config adjustments

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-	id("org.jetbrains.kotlin.jvm") version "1.9.23"
-	id("org.jetbrains.kotlin.plugin.spring") version "1.9.23"
-	id("org.jetbrains.kotlin.plugin.jpa") version "1.9.23"
+	id("org.jetbrains.kotlin.jvm") version "2.3.10"
+	id("org.jetbrains.kotlin.plugin.spring") version "2.3.10"
+	id("org.jetbrains.kotlin.plugin.jpa") version "2.3.10"
 
 	id("org.springframework.boot") version "3.2.5"
 	id("io.spring.dependency-management") version "1.1.4"
